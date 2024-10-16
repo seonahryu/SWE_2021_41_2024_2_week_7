f46168b036a8b41fa023539bc93d69d03744ef12
seonahryu <fbtjsdk0818@naver.com>
2024-10-16 13:15:24 +0000

    'twoSum' function in 'main.py'

--- main.py
+++ main.py
@@ -10,7 +10,15 @@ def twoSum(nums: List[int], target: int) -> List[int]:
                 return ans
     return ans
 
+#Test cases
+print(f"Test case 1 Output: {twoSum([2,7,11,15], 9)}")
+print(f"Test case 2 Output: {twoSum([3,2,4], 6)}")
+print(f"Test case 3 Output: {twoSum([3,3], 6)}")
+
+"""
 nums = input()
-nums=eval(nums)
+nums = eval(nums)
 target = int(input())
 print(twoSum(nums, target))
+
+"""
